52c9fd46fde230a6ff1ec95ae0b6a8ed0f30173b
AviCafre <mar7benitez@gmail.com>
2023-11-10 13:29:16 -0300

    responsive

---
+++ script.js
@@ -0,0 +1,8 @@
+/* function toggleNav() {
+    var nav = document.getElementById("mainNav");
+    if (nav.style.display === "none" || nav.style.display === "") {
+        nav.style.display = "block";
+    } else {
+        nav.style.display = "none";
+    }
+} */
